a9088de3d25f63301e941343e473ebb0dee27be9
youngjong <dudwhd96@naver.com>
2025-12-24 10:51:33 +0900

    settings

--- back/Syncmovie/settings.py
+++ back/Syncmovie/settings.py
@@ -43,11 +43,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1', 
-    'localhost', 
-    'https://port-0-syncmovie-mjj9gba6c7a2c78b.sel3.cloudtype.app',
-    ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
